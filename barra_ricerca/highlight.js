d28ae38bb9bfcf895138eb23f1cc2269239c582b
Emanuele Urso <ursomanuele@gmail.com>
2026-01-11 23:25:05 +0100

    barra di ricerca (che ancora non funziona)

---
+++ barra_ricerca/highlight.js
@@ -0,0 +1,15 @@
+function highlight(text, query) {
+  const regex = new RegExp(`(${query})`, "gi");
+  return text.replace(regex, "<mark>$1</mark>");
+}
+
+function makeSnippet(text, query) {
+  const index = text.toLowerCase().indexOf(query);
+  if (index === -1) return text.slice(0, 120) + "...";
+
+  const start = Math.max(0, index - 40);
+  const end = index + 80;
+
+  const snippet = text.slice(start, end);
+  return highlight("..." + snippet + "...", query);
+}
