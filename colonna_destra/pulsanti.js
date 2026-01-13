cfcf0d23344aca4ee826c19cf5d33cedfb4f8d05
Emanuele Urso <ursomanuele@gmail.com>
2026-01-13 16:59:14 +0100

    cerco di sistemare la modularità

--- colonna_destra/pulsanti.js
+++ colonna_destra/pulsanti.js
@@ -1,47 +1,21 @@
-let pages = [];
+const button = document.querySelector("button");
+button.addEventListener("click", changeMultipleCSS);
 
+function changeMultipleCSS(e) {
+  // Defining all our CSS styles
+  const myStyles = `
+    display: block;
+    width: 80%;
+    background-color: red;
+    border: 2px;
+    font-size: 5em;
+    color: white;
+    margin: 20px;
+    padding-left: 10px;
+    padding-bottom: 10px;
+    border: 2px solid black;
+  `;
+  const element = document.querySelector(".demo");
 
-const input = document.getElementById("font");
-console.log(input)
-
-input.addEventListener("input", () => {
-  const query = input.value.toLowerCase();
-  console.log(query)
-  results.innerHTML = "";
-
-  if (query.length < 1) {
-    results.style.display = "none";
-    return;
-  }
-
-  const matches = pages.filter(p =>
-    p.title.toLowerCase().includes(query) ||
-    p.content.toLowerCase().includes(query)
-  );
-
-  if (matches.length === 0) {
-    results.style.display = "none";
-    return;
-  }
-
-  matches.slice(0, 8).forEach(p => {
-    const div = document.createElement("div");
-    div.className = "result";
-    div.innerHTML = `
-      <a href="${p.url}">
-        <strong>${highlight(p.title, query)}</strong><br>
-        <small>${makeSnippet(p.content, query)}</small>
-      </a>
-    `;
-    results.appendChild(div);
-  });
-
-  results.style.display = "block";
-});
-
-/* chiudi cliccando fuori */
-document.addEventListener("click", e => {
-  if (!e.target.closest(".search-box")) {
-    results.style.display = "none";
-  }
-});
+  element.style.cssText = myStyles;
+}
