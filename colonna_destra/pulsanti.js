e47af88fcb844fa5c7cd48ef41ff9ec16561d106
Emanuele Urso <ursomanuele@gmail.com>
2026-01-14 12:35:48 +0100

    modifiche minori

--- colonna_destra/pulsanti.js
+++ colonna_destra/pulsanti.js
@@ -28,7 +28,8 @@ function HighContrast(){
   document.querySelector(':root').style.setProperty('--text', '#ffffff');
   document.querySelector(':root').style.setProperty('--primary', '#00ffff');
   document.querySelector(':root').style.setProperty('--link', '#ffff00');
-  document.querySelector(':root').style.setProperty('--background', '#000000');
+  document.querySelector(':root').style.setProperty('--background', '#1f1f1f');
+  document.querySelector(':root').style.setProperty('--button-background', '#ffffff');
 }
 
 function StandardContrast(){
@@ -36,6 +37,7 @@ function StandardContrast(){
   document.querySelector(':root').style.setProperty('--primary', '#70193D');
   document.querySelector(':root').style.setProperty('--link', '#191970');
   document.querySelector(':root').style.setProperty('--background', '#1919704F');
+  document.querySelector(':root').style.setProperty('--button-background', '#000000');
 }
 
 function BigText(){
