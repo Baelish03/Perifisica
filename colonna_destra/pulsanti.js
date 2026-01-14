52ee038ac26d3ab91abdc5f2772e17bfdb65feb5
Emanuele Urso <ursomanuele@gmail.com>
2026-01-14 01:54:36 +0100

    Update pulsanti.js

--- colonna_destra/pulsanti.js
+++ colonna_destra/pulsanti.js
@@ -1,7 +1,28 @@
 function OpenDyslexicFunc() {
   document.querySelector('body').style.fontFamily = 'OpenDyslexic, serif';
+  /*const titles = ['h1', 'h2', 'h3', 'h4'];
+  for (let x = 0; x < titles.length; i++) {
+    const elements = document.querySelectorAll(titles[i]);
+    for (let i = 0; i < elements.length; i++) {
+      elements[i].style.fontFamily = 'OpenDyslexic, serif';
+    }
+  }*/
 }
 
 function StandardFont() {
-  document.querySelector('body').style.fontFamily = 'Inter, sans-serif';
+  document.querySelector('body').style.fontFamily = '';
+  /*const titles = ['h1', 'h2', 'h3', 'h4'];
+  for (let x in titles) {
+    const elements = document.querySelectorAll(titles[x]);
+    for (let i in elements) {
+      elements[i].style.fontFamily = 'Inter, sans-serif';
+    }
+  }*/
 }
+  /*document.querySelectorAll('h1').style.fontFamily = 'Inter, sans-serif';
+  document.querySelectorAll('h2').style.fontFamily = 'Inter, sans-serif';
+  document.querySelectorAll('h3').style.fontFamily = 'Inter, sans-serif';
+  document.querySelectorAll('h4').style.fontFamily = 'Inter, sans-serif';*/
+
+  
+
