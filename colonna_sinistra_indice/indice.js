6f9256d2807af7f11ba8d56a21775ae3d21bdbde
Emanuele Urso <ursomanuele@gmail.com>
2026-01-16 00:42:55 +0100

    risolta la condition race per l'indice

--- colonna_sinistra_indice/indice.js
+++ colonna_sinistra_indice/indice.js
@@ -1,34 +1,44 @@
+function Chevron(contentId, chevronId) {
+  const content = document.querySelector("#" + contentId);
+  const chevron = document.querySelector("#" + chevronId);
+
+  console.log(content);
+
+  const isOpen = content.style.maxHeight;
+  console.log(isOpen);
+  if (isOpen) {
+    content.style.maxHeight = null;
+    chevron.style.transform = "rotate(0deg)";
+    chevron.style.setProperty("left", "0rem");
+    chevron.style.setProperty("top", "-.5rem");
+  } else {
+    content.style.maxHeight = content.scrollHeight + "px";
+    chevron.style.transform = "rotate(90deg)";
+    chevron.style.setProperty("left", ".5rem");
+    chevron.style.setProperty("top", "0.5px");
+  }
+}
+
+
 /* When the user clicks on the button, 
 toggle between hiding and showing the dropdown content */
 function OpenTeoria() {
-  document.getElementById("teoria").classList.toggle("show");
+  document.getElementById("teoria-button").classList.toggle("show");
+  Chevron("teoria-content", "teoria-chevron");
 }
 
 function OpenEsperimenti() {
-  document.getElementById("esperimenti").classList.toggle("show");
+  document.getElementById("esperimenti-button").classList.toggle("show");
+  Chevron("esperimenti-content", "esperimenti-chevron");
 }
 
 function OpenLatex() {
-  document.getElementById("latex").classList.toggle("show");
+  document.getElementById("latex-button").classList.toggle("show");
+  Chevron("latex-content", "latex-chevron");
 }
 
 /*
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbutton')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-  */
-
-//function Chevron() {
+function Chevron() {
   document.querySelectorAll(".accordion").forEach(accordion => {
     const button = accordion.querySelector(".dropbutton");
     const content = accordion.querySelector(".dropdown-content");
@@ -50,4 +60,15 @@ window.onclick = function(event) {
       }
     });
   });
-//}
+}*/
+
+export function init(root) {
+  const teoria = root.querySelector("#teoria-button");
+  teoria?.addEventListener("click", OpenTeoria);
+
+  const esperimenti = root.querySelector("#esperimenti-button");
+  esperimenti?.addEventListener("click", OpenEsperimenti);
+
+  const latex = root.querySelector("#latex-button");
+  latex?.addEventListener("click", OpenLatex);
+}
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import { includeHTML } from "./include_html.js";
 import { Search } from "/barra_ricerca/search.js";
 import { init as Pulsanti } from "/colonna_destra/pulsanti.js"
-//import { init as Indice } from "/colonna_sinistra_indice/indice.js"
+import { init as Indice } from "/colonna_sinistra_indice/indice.js"
 
 
 
@@ -9,6 +9,6 @@ await includeHTML();
 
 Search();
 Pulsanti(document);
-//Indice();
+Indice(document);
 
 
