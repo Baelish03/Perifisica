c03cf4a8935b6ddc3067229a6471907648553086
Emanuele Urso <ursomanuele@gmail.com>
2026-01-15 23:37:53 +0100

    race condition risolta per la colonna destra

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import { includeHTML } from "./include_html.js";
 import { Search } from "/barra_ricerca/search.js";
-//import { init as Pulsanti } from "/colonna_destra/pulsanti.js"
+import { init as Pulsanti } from "/colonna_destra/pulsanti.js"
 //import { init as Indice } from "/colonna_sinistra_indice/indice.js"
 
 
@@ -8,7 +8,7 @@ import { Search } from "/barra_ricerca/search.js";
 await includeHTML();
 
 Search();
-//Pulsanti();
+Pulsanti(document);
 //Indice();
 
 
