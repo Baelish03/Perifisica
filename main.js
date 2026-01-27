5bb35a8fdedfb6fd6e0a50ded6b52c77b0de17c3
Emanuele Urso <ursomanuele@gmail.com>
2026-01-27 17:35:27 +0100

    Revert "modifiche minori"
    This reverts commit b958d3ed0ac2f07282af8cf3ab0201a9fa107dab.

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 import { includeHTML } from "./include_html.js";
-import { Search } from "./barra_ricerca/search.js";
-import { init as Pulsanti } from "./colonna_destra/pulsanti.js"
-import { init as Indice } from "./colonna_sinistra_indice/indice.js"
-import { init as Mobile } from "./mobile/mobile.js";
+import { Search } from "/barra_ricerca/search.js";
+import { init as Pulsanti } from "/colonna_destra/pulsanti.js"
+import { init as Indice } from "/colonna_sinistra_indice/indice.js"
+import { init as Mobile } from "/mobile/mobile.js";
 import { init as Allarga } from "./image_enlarge.js";
 
 await includeHTML();
