48e957a694b342a101249c20697096ee280b1121
Emanuele Urso <ursomanuele@gmail.com>
2026-02-07 11:56:42 +0100

    Update image_enlarge.js
    Non apre più l'immagine del logo che fa da link per la pagina principale ed ora l'immagine si chiude con la X

--- image_enlarge.js
+++ image_enlarge.js
@@ -10,26 +10,54 @@ function AllargaImmagine() {
     z-index: 9999;
   `;
 
+  const cross = document.createElement("button");
+  cross.innerHTML = "&#x2715;";
+  cross.style.cssText = `
+    position: absolute;
+    right: 10px;
+    top: 10px;
+
+    background: transparent;
+    color: var(--primary);
+    border-color: transparent;
+    font-size: xxx-large;
+
+    cursor: pointer;
+  `;
+
   const img = document.createElement("img");
   img.style.cssText = `
     max-width: 100%;
     max-height: 100%;
     object-fit: contain;
+    position: absolute;
   `;
 
   overlay.appendChild(img);
+  overlay.appendChild(cross);
   document.body.appendChild(overlay);
 
   document.querySelectorAll("img").forEach(i => {
-    i.addEventListener("click", () => {
-      img.src = i.src;
-      overlay.style.display = "flex";
-    });
+    const img_id = i.getAttribute('id');
+    if (img_id === 'foto_logo') {
+      return
+    }
+    else {
+      i.addEventListener("click", () => {
+        img.src = i.src;
+        overlay.style.display = "flex";
+      });
+    };
   });
 
-  overlay.addEventListener("click", () => {
+
+  cross.addEventListener("click", () => {
     overlay.style.display = "none";
   });
+
+  /*overlay.addEventListener("click", () => {
+    overlay.style.display = "none";
+  });*/
 
   document.addEventListener("keydown", e => {
     if (e.key === "Escape") overlay.style.display = "none";
