cb114f2f7214a43a3c163342821013d5cf924d79
Emanuele Urso <ursomanuele@gmail.com>
2026-04-17 01:10:48 +0200

    pop over funzionante per le note a margine

--- pop_over.js
+++ pop_over.js
@@ -1,54 +1,46 @@
 function PopOver() {
-  const overlay = document.createElement("div");
-  overlay.style.cssText = `
-    position: absolute;
-    background: white;
-    display: none;
-    justify-content: center;
-    align-items: center;
-    z-index: 9999;
-    height: 1rem;
-    width: 1rem;
-  `;
+  var text_css =
+    `height : auto;
+      width : auto;
+      max-width : 15vw;
+      display: inline-block;
+      position: absolute;
+      background: var(--background-color);
+      z-index: 9999;
+      bottom: 1em;
+      cursor: initial;
+      `;
 
-  const sidenote_text = document.querySelectorAll(".sidenote_text")[0];
-
-  const text = document.createElement("p");
-  text.style.cssText = `
-    max-width: 100%;
-    max-height: 100%;
-    object-fit: contain;
-    position: absolute;
-  `;
-  text.innerHTML = sidenote_text.textContent;
-
-  overlay.appendChild(text);
-  document.body.appendChild(overlay);
+      // fai un bel box
 
   document.querySelectorAll(".sidenote").forEach(sup => {
-    sup.style.cssText = 'cursor: pointer;';
+    sup.style.cssText =
+      `cursor: pointer;
+      position: relative`;
 
-    //const sup_top = sup.getBoundingClientRect().top;
-    //const sup_left = sup.getBoundingClientRect().left; NON FUNZIONA, DÀ LA POSIZIONE RISPETTO ALLO SCHERMO
+    const span = sup.children[0]
 
-    console.log(sup_top);
+    sup.addEventListener("click", (e) => {
+      e.stopPropagation();
+      if (span.style.display === "none") {
+        span.style.cssText = text_css;
+      }
+      else {
+        span.style.display = "none";
+      }
+    });
 
-    sup.addEventListener("click", () => {
-      overlay.style.display = "flex";
-      //overlay.style.left = sup_left + "px";
-      //overlay.style.top = sup_top + "px";
+    document.addEventListener("click", e => {
+      if (!span.contains(e.target) && !e.target.closest(".sidenote")) {
+        span.style.display = "none";
+      }
+    });
+
+    document.addEventListener("keydown", e => {
+      if (e.key === "Escape") span.style.display = "none";
     });
   });
 
-  document.addEventListener("click", e => {
-    if (!overlay.contains(e.target) && !e.target.closest(".sidenote")) {
-      overlay.style.display = "none";
-    }
-  });
-
-  document.addEventListener("keydown", e => {
-    if (e.key === "Escape") overlay.style.display = "none";
-  });
 }
 
 export function init() {
