4f7d8a7ed6144d54520dd0ce78ee7ffa33e6a2bc
Emanuele Urso <97971495+Baelish03@users.noreply.github.com>
2026-04-24 15:49:54 +0200

    Refactor ApriIndice and ApriAccessibilità functions

--- mobile/mobile.js
+++ mobile/mobile.js
@@ -1,44 +1,28 @@
 function ApriIndice() {
-  const indice = document.getElementById("indice");
-  indice.classList.toggle("indice_aperto");
-
-  const dropdown = document.getElementById("dropdown");
-  dropdown.classList.toggle("dropdown_aperto");
-
-  document.addEventListener("click", e => {
-    if (!e.target.closest("#indice") && !e.target.closest("#apri-indice-mobile")) {
-      indice.classList.remove("indice_aperto");
-      dropdown.classList.remove("dropdown_aperto");
-    }
-  });
-
-  document.addEventListener("keydown", e => {
-    if (e.key === "Escape") {
-      indice.classList.remove("indice_aperto");
-      dropdown.classList.remove("dropdown_aperto");
-    }
-  });
+  document.getElementById("indice").classList.toggle("indice_aperto");
+  document.getElementById("dropdown").classList.toggle("dropdown_aperto");
 }
-
 
 function ApriAccessibilità() {
-  const sidebar_right = document.getElementById("sidebar_right");
-  sidebar_right.classList.toggle("right_aperta");
+  document.getElementById("sidebar_right").classList.toggle("right_aperta");
+}
 
-  document.addEventListener("click", e => {
-    if (!e.target.closest("#sidebar_right") && !e.target.closest("#apri-accessibilità-mobile")) {
-      sidebar_right.classList.remove("right_aperta");
-    }
-  });
+document.addEventListener("click", e => {
+  if (!e.target.closest("#indice") && !e.target.closest("#apri-indice-mobile")) {
+    document.getElementById("indice").classList.remove("indice_aperto");
+    document.getElementById("dropdown").classList.remove("dropdown_aperto");
+  }
+  if (!e.target.closest("#sidebar_right") && !e.target.closest("#apri-accessibilità-mobile")) {
+      document.getElementById("sidebar_right").classList.remove("right_aperta");
+});
 
   document.addEventListener("keydown", e => {
     if (e.key === "Escape") {
-      sidebar_right.classList.remove("right_aperta");
+      document.getElementById("indice").classList.remove("indice_aperto");
+      document.getElementById("dropdown").classList.remove("dropdown_aperto");
+      document.getElementById("sidebar_right").classList.remove("right_aperta");
     }
   });
-}
-
-
 
 
 function posiziona() {
