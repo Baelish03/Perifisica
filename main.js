64efbfb826cf694a11f9e215778d49651389004c
Emanuele Urso <ursomanuele@gmail.com>
2026-06-18 22:45:11 +0200

    katek al posto di mathjax
    è molto più leggero

--- main.js
+++ main.js
@@ -1,39 +1,29 @@
-window.MathJax = {
-  tex: {
-    inlineMath: [['$', '$'], ['\\(', '\\)']],
-    displayMath: [['$$', '$$'], ['\\[', '\\]']],
-  },
-  svg: {
-    fontCache: 'global'
-  },
-  startup: {
-    ready() {
-      MathJax.startup.defaultReady();
-    }
-  }
-};
-
-
-// Caricamento dinamico MathJax
-function loadMathJax() {
-  return new Promise((resolve) => {
-    const script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-svg.js';
-    script.async = true;
-    script.onload = resolve;
-    document.head.appendChild(script);
+function loadScript(src) {
+  return new Promise((resolve, reject) => {
+    const s = document.createElement('script');
+    s.src = src;
+    s.onload = resolve;
+    s.onerror = reject;
+    document.head.appendChild(s);
   });
 }
-
 
 import { includeHTML } from "./include_html.js";
 await includeHTML();
 
-// Carica MathJax e renderizza le formule già presenti nel DOM
-await loadMathJax();
-await MathJax.typesetPromise();
+// KaTeX: caricamento sequenziale in parallelo col Promise.all
+const katexReady = loadScript('/katex/katex.min.js')
+  .then(() => loadScript('/katex/contrib/auto-render.min.js'))
+  .then(() => renderMathInElement(document.body, {
+    delimiters: [
+      { left: '$$', right: '$$', display: true },
+      { left: '$', right: '$', display: false },
+      { left: '\\(', right: '\\)', display: false },
+      { left: '\\[', right: '\\]', display: true }
+    ]
+  }));
 
-const [Search, Pulsanti, Indice, Mobile, Allarga, PopOver, InlineNav, StickyTitles] = 
+const [Search, Pulsanti, Indice, Mobile, Allarga, PopOver, InlineNav, StickyTitles] =
   await Promise.all([
     import("./barra_ricerca/search.js"),
     import("./colonna_destra/pulsanti.js"),
@@ -42,7 +32,8 @@ const [Search, Pulsanti, Indice, Mobile, Allarga, PopOver, InlineNav, StickyTitl
     import("./image_enlarge.js"),
     import("./pop_over.js"),
     import("./sticky_titles.js"),
-    import("./inline_navigation/inline_navigation.js")]);
+    import("./inline_navigation/inline_navigation.js"),
+    katexReady]);
 
 Search.init();
 Pulsanti.init(document);
